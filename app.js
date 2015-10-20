fd08c6c002d06478ec3f23a79d81318f7ea9f795
Pascal Beynel <pascal@beynel.fr>
2015-10-20 12:18:47 +0200

    Work in progress

--- app.js
+++ app.js
@@ -1,266 +1,312 @@
-!(function($) {
 
-  /*******************************************************
-  *
-  *
-  * Init
-  *
-  *
-  *******************************************************/
-
-  var $origin = $('#origin'),
-      $destination = $('#destination'),
-      $alert = $('.alert');
-
-  /*******************************************************
-  *
-  *
-  * Events
-  *
-  *
-  *******************************************************/
-
-  /*
-   *
-   * Valid google form
-   *
-   */ 
-
-  $(document).on('click', '.valid', function(e){
+$(function() {
     
-    if ($origin.val() != null && $destination.val() != null){
-      getTravalData(google.maps.TravelMode.BICYCLING);
-      getTravalData(google.maps.TravelMode.DRIVING);
-      getTravalData(google.maps.TravelMode.TRANSIT);
-      getTravalData(google.maps.TravelMode.WALKING);
-    }
+    /**************************************************
+    *
+    * Vars
+    *
+    **************************************************/
 
-    e.preventDefault();
-  });
+    var $inputRange = $('input[type="range"]');
 
-  /*
-   *
-   * Launch calcul
-   *
-   */ 
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
 
-  $(document).on('click', '.calcul', function(e){
-  
-    socialSpeed('bicycling');
-    socialSpeed('driving');
-    socialSpeed('transit');
-    socialSpeed('walking');
+    // I put duration, distance and specialtrip as general variables because I don't know how to extract them or pass them from a function to another
+    var distance = 0;
+    var duration = 0;
+    var specialtrip = 1;
 
-    e.preventDefault();
-  });
+    /**************************************************
+    *
+    * Init
+    *
+    **************************************************/
 
-  /*******************************************************
-  *
-  *
-  * Functions
-  *
-  *
-  *******************************************************/
+    // Init google map
+    google.maps.event.addDomListener(window, 'load', initGoogleMap);
 
-  /*
-   *
-   * Google API call
-   * https://developers.google.com/maps/documentation/javascript/distancematrix#transit_options
-   *
-   */ 
+    // Init slider
+    $inputRange.rangeslider({
+      polyfill: false 
+    });
 
-  function getTravalData(mode){
+    // Init slider range value
+    $inputRange.each(function(){
+      valueOutput($(this));
+    });
 
-    var origin = "Greenwich, England";
-    var destination = "Stockholm, Sweden";
-    var service = new google.maps.DistanceMatrixService();
+    /**************************************************
+    *
+    * Functions
+    *
+    **************************************************/
 
-    service.getDistanceMatrix(
-      {
-        origins: [$origin.val()],
-        destinations: [$destination.val()],
-        travelMode: mode,
-        durationInTraffic: true,
-      }, callback);
+    /*
+     *
+     * GoogleMap function
+     *
+     */
 
-    function callback(response, status) {
-      if (status != 'OK') {
-        displayErrorCall(status);
-      } else {
-        showResultsCall(mode, response);
-      }
-    }
-  }
+    function initGoogleMap(){
 
-  /*
-   *
-   * Google API call errors
-   *
-   */ 
+      var markers = [];
 
-  function displayErrorCall(status){
-    
-    var message = '';
+      directionsDisplay = new google.maps.DirectionsRenderer();
+      directionsDisplay.setMap(map);
 
-    if (status == 'INVALID_REQUEST'){
-      message = 'A field is missing';
-    } else if (status == 'MAX_ELEMENTS_EXCEEDED'){
-      message = 'A field is missing';
-    } else if (status == 'MAX_DIMENSIONS_EXCEEDED'){
-      message = 'Too much origins and destinations';
-    } else if (status == 'OVER_QUERY_LIMIT'){
-      message = 'Too many requests in allowed time period. Wait a little before retrying';
-    } else if (status == 'REQUEST_DENIED'){
-      message = 'Service denied by google';
-    } else if (status == 'UNKNOWN_ERROR'){
-      message = 'Server error. Please try again';
-    } else if (status == 'NO_SALARY'){
-      message = 'Please provide your hourly salary';
-    }
+      // Options
+      var mapOptions = {
+        zoom: 7,
+        center: new google.maps.LatLng(41.850033, -87.6500523)
+      };
 
-    $alert
-      .children('.message')
-      .html(message)
-      .end()
-    .show();
-
-  }
-
-  /*
-   *
-   * Google API response Error
-   *
-   */ 
-
-  function displayErrorResponse(status, mode){
-    
-    var message = '';
-
-    if (status == 'NOT_FOUND'){
-      message = 'The origin and/or destination of this pairing could not be geocoded.';
-    } else if (status == 'ZERO_RESULTS'){
-      message = 'No route could be found between the origin and destination.';
-    }
-
-    $('input.distance.' + mode)
-            .val(message)
-            .parent()
-              .addClass('has-error has-feedback')
-              .append('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
-  }
-
-  /*
-   *
-   * Show Google API response
-   *
-   */ 
-
-  function showResultsCall(mode, response){
-
-    var item = response.rows[0].elements[0],
-        mode = mode.toLowerCase();
-  
-    if (item.status != 'OK'){
-      displayErrorResponse(item.status, mode);
-    } else {
-      $('input.distance.' + mode)
-        .val(item.distance.text)
-        .attr('data-raw', item.distance.value)
-        .parent()
-          .addClass('has-success has-feedback')
-          .append('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
-
-      $('input.duration.' + mode)
-        .val(item.duration.text)
-        .attr('data-raw', item.duration.value)
-        .parent()
-          .addClass('has-success has-feedback')
-          .append('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');   
-    }
-  }
-
-  /*
-   *
-   * Calcul social speed
-   *
-   */
-
-  function socialSpeed(mode){
+      // map initialization  
+      var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+      directionsDisplay.setMap(map);
       
-      var tripTime = parseFloat($('.duration.' + mode).attr('data-raw')),
-          tripDistance = parseFloat($('.distance.' + mode).attr('data-raw')),
-          tripCost = getCost(mode),
-          hourlySalary = parseFloat($('#hourlysalary').val());
-
-      if (hourlySalary == null) {
-
-        displayErrorCall('NO_SALARY')
-
-      } else {
-
-        // Conveting time to hours :
-        // trip time is in second => division by 60 to get minutes
-        // hourly salary is in hour => mutliply by 60 to have minutes
-        generalizedTime = (tripTime / 60) + (tripCost / hourlySalary * 60);
-        // Finally we have hours
-        generalizedTime = generalizedTime / 60;
-        // Converting distance into Km
-        generalizedSpeed = tripDistance / 1000 / generalizedTime;
-
-        $('.generalizedtime.' + mode).val(toHHMM(generalizedTime * 3600));
-        $('.generalizedspeed.' + mode).val(generalizedSpeed.toFixed(1) + ' km/h');
+      // center map on user location
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          map.setCenter(initialLocation);
+        });
       }
-  } 
 
-  /*
-   *
-   * Calcul total cost for transport mode
-   *
-   */
+      // Create a search box with autocomplete and link it to the UI element.
+      var searchBox = new google.maps.places.SearchBox((document.getElementById('origin')));
+      var searchBox2 = new google.maps.places.SearchBox((document.getElementById('destination')));
 
-  function getCost(mode){
-      
-      var total = 0;
-
-      $('.cost.' + mode).each(function(){
-        
-        var $this = $(this),
-            subtotal = 0;
-
-        if ($this.val() !== undefined && $this.val() !== '') {
-          subtotal = parseFloat($this.val());
-        }
-
-        // Is there something that minor the cost ?
-        // for example, if you take your car twice a day, we can consider you take it twice all days of years, so your journey costs will be divided by 365 * 2
-        if ($this.attr('data-minor') !== undefined && $this.attr('data-minor') !== '') {
-          subtotal = subtotal / parseFloat($this.attr('data-minor'));
-        }
-
-        total = parseFloat(total) + parseFloat(subtotal);
+      // Bias the SearchBox results towards places that are within the bounds of the
+      // current map's viewport.
+      google.maps.event.addListener(map, 'bounds_changed', function() {
+        var bounds = map.getBounds();
+        searchBox.setBounds(bounds);
       });
 
-      return parseFloat(total);
-  } 
+    }
 
-  /*
-   *
-   * Time convertion
-   *
-   */
+    /*
+     *
+     * GoogleMap route function
+     *
+     */
 
-  function toHHMM(time) {
-    
-    var sec_num = parseInt(time, 10); // don't forget the second param
-    var hours   = Math.floor(sec_num / 3600);
-    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    function googleMapRoute() {
+      
+      var start = $('#origin').val(),
+          end = $('#destination').val(),
+          selectedMode = $('#mode').val();
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    
-    var time    = hours + ' h '+ minutes+' min';
-    return time;
-  }
+      if (start != "" && end != "" && selectedMode != null) {
 
+        if (selectedMode.substr(0, 7) == "DRIVING") {
+          selectedMode = "DRIVING";
+        }
 
-})(window.jQuery);
+        var request = {
+            origin: start ,
+            destination: end,
+            travelMode: google.maps.TravelMode[selectedMode]
+        };
+
+        directionsService.route(request, function(response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+          }
+        });
+        
+        // Calculate distance and duration for the trip
+        var service = new google.maps.DistanceMatrixService();
+        service.getDistanceMatrix(
+         {
+           origins: [start],
+           destinations: [end],
+           travelMode: google.maps.TravelMode[selectedMode],
+           unitSystem: google.maps.UnitSystem.METRIC,
+           avoidHighways: false,
+           avoidTolls: false
+         }, googleMapCallback);
+      }
+    }
+
+    /*
+     *
+     * GoogleMap route function
+     *
+     */
+
+    function googleMapCallback(response, status) {
+
+      if (status != google.maps.DistanceMatrixStatus.OK) {
+        
+        console.log('Error was: ' + status);
+
+      } else {
+        
+        var origins = response.originAddresses,
+            destinations = response.destinationAddresses,
+            $outputDiv = $('#outputDiv'),
+            html = '';
+        
+        $outputDiv.html('');
+
+        for (var i = 0; i < origins.length; i++) {
+          var results = response.rows[i].elements;
+
+          for (var j = 0; j < results.length; j++) {
+            // display the trip characteristics to check
+            html += origins[i] + ' to ' + destinations[j]
+                + ': ' + results[j].distance.text + ' in '
+                + results[j].duration.text;
+            // values for distance and directions in meters and seconds         
+            distance = results[j].distance.value;
+            duration = results[j].duration.value;
+            html += '(' + duration + ')';
+          }
+
+          // update div
+          $outputDiv.html(html);
+
+        }
+      }
+
+      updateResult();
+    }
+
+    /*
+     *
+     * Update result function
+     *
+     */
+
+    function updateResult() {
+
+      var $socialtime = $("#socialtime"),
+          $socialspeed = $("#socialspeed"),
+          $cost = $("#cost"),
+          $wage = $("#wage"),
+          $costout = $("#costout"),
+          $wageout = $("#wageout"),
+          selectedMode = $('#mode').val();
+      
+      var num1 = parseFloat($cost.val());
+      var num2 = 1;
+
+      // Formula depending on the transport mode
+      if (selectedMode != null) {
+
+        if (selectedMode.substr(0, 7) == "DRIVING") {
+
+          waitingTime = 0;
+          num2 = parseFloat($wage.val());
+
+          if (selectedMode  == "DRIVING_HITCH") {
+            specialtrip = 99999;
+            waitingTime = parseFloat($("#waiting").val())*60;
+          } else if (selectedMode  == "DRIVING_SHARE2") {
+            specialtrip = 2;
+          } else if (selectedMode  == "DRIVING_SHARE3") {
+            specialtrip = 3;
+          } else if (selectedMode  == "DRIVING_SHARE4") {
+            specialtrip = 4;
+          } else if (selectedMode  == "DRIVING_SHARE5") {
+            specialtrip = 5;
+          }
+
+          //----------------------------------  
+          // formula for the generalized time in minutes. need to convert it to days and hours and minutes..
+          var num3 = ((duration+waitingTime+((distance*num1)/specialtrip)/num2)/60).toFixed(0);
+
+          //----------------------------------  
+          // formula for the generalized speed
+          var num4 = (distance/(duration+waitingTime+(distance*num1)/specialtrip/num2)*3.6).toFixed(0);
+
+        } else if (selectedMode  == "WALKING") {
+
+          /*
+            TODO
+          */
+
+          //----------------------------------  
+          // formula for the generalized time in minutes. need to convert it to days and hours and minutes..
+          var num3 = ((duration+((distance*num1)/specialtrip)/num2)/60).toFixed(0);
+
+          //----------------------------------  
+          // formula for the generalized speed
+          var num4 = (distance/(duration+(distance*num1)/specialtrip/num2)*3.6).toFixed(0);
+
+        } else if (selectedMode  == "BICYCLING") {
+
+          /*
+            TODO
+          */
+
+          //----------------------------------  
+          // formula for the generalized time in minutes. need to convert it to days and hours and minutes..
+          var num3 = ((duration+((distance*num1)/specialtrip)/num2)/60).toFixed(0);
+
+          //----------------------------------  
+          // formula for the generalized speed
+          var num4 = (distance/(duration+(distance*num1)/specialtrip/num2)*3.6).toFixed(0);
+
+        } else if (selectedMode  == "TRANSIT") {
+
+          /*
+            TODO
+          */
+
+          num1 = $("#transitprice").val();
+
+          //----------------------------------  
+          // formula for the generalized time in minutes. need to convert it to days and hours and minutes..
+          var num3 = ((duration+((distance*num1)/specialtrip)/num2)/60).toFixed(0);
+
+          //----------------------------------  
+          // formula for the generalized speed
+          var num4 = (distance/(duration+(distance*num1)/specialtrip/num2)*3.6).toFixed(0);
+
+        }  
+
+        $socialtime.html(num3);
+        $socialspeed.html(num4);
+      }
+    }
+
+    /*
+     *
+     * Update range value
+     *
+     */
+
+    function valueOutput(element) {
+        var $this = $(element),
+            $output = $this.parent().find('.output');
+
+        // output html update
+        $output.html($this.val());
+    }
+
+    /**************************************************
+    *
+    * Events
+    *
+    **************************************************/
+
+    $(document).on("change", "#origin, #destination", function(e){
+      googleMapRoute();
+    });
+
+    $(document).on("change", "#mode", function(e){
+      googleMapRoute();
+      $(".transport.option").hide();
+      $(".transport.option." + $(this).val().toLowerCase()).show();
+    });
+
+    $(document).on("change", 'input[type="range"], .transport.option input', function(e) {
+      valueOutput(e.target);
+      updateResult();
+    });
+
+});
+
